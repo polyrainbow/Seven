b6d9cf27395d6bd09b9f541fdfc5480e7c532b52
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2017-10-27 12:41:24 +0200

    New preset: Groundhog day

--- reducers/presets.js
+++ reducers/presets.js
@@ -781,6 +781,239 @@ var cooper = {
 	}
 };
 
+
+var groundhog_day = {
+    "name": "Groundhog Day",
+	"data": {
+		"paths": [
+			{
+				"id": "6fbdd911-a7db-4ccd-966e-597f8898b178",
+				"startTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"endTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"dilationFactor": 1,
+				"universe_index": 0,
+				"durationInRS1": 86400000,
+				"description": "This ist the first time that Phil experiences the 2nd February 1993.",
+				"isInactive": false,
+				"relativeStartRS1": 0,
+				"relativeEndRS1": 0.25,
+				"relativeStartRS2": 0,
+				"relativeEndRS2": 1
+			},
+			{
+				"id": "e5c8d33a-dccf-49f9-b51a-06cb7521ad75",
+				"startTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"endTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"dilationFactor": 1,
+				"universe_index": 1,
+				"durationInRS1": 86400000,
+				"description": "",
+				"isInactive": false,
+				"relativeStartRS1": 0.25,
+				"relativeEndRS1": 0.5,
+				"relativeStartRS2": 0,
+				"relativeEndRS2": 1
+			},
+			{
+				"id": "351543b6-99bf-4ad3-bd4c-c8c9209efec3",
+				"startTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"endTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"dilationFactor": 1,
+				"universe_index": 2,
+				"durationInRS1": 86400000,
+				"description": "",
+				"isInactive": false,
+				"relativeStartRS1": 0.5,
+				"relativeEndRS1": 0.75,
+				"relativeStartRS2": 0,
+				"relativeEndRS2": 1
+			},
+			{
+				"id": "ebe9e87d-0b8e-48f0-8a87-105a479fc6f4",
+				"startTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"endTime": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"dilationFactor": 1,
+				"universe_index": 3,
+				"durationInRS1": 86400000,
+				"description": "",
+				"isInactive": false,
+				"relativeStartRS1": 0.75,
+				"relativeEndRS1": 1,
+				"relativeStartRS2": 0,
+				"relativeEndRS2": 1
+			}
+		],
+		"RS1duration": 345600000,
+		"universes": [
+			{
+				"id": "945d445f-5d12-4224-af27-1ed0bf41527c",
+				"name": "1st iteration",
+				"description": "",
+				"earliestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"latestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"RS2duration": 86400000
+			},
+			{
+				"id": "f86906d0-d206-40ee-b80f-f405f7580c43",
+				"name": "2nd iteration",
+				"description": "",
+				"earliestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"latestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"RS2duration": 86400000
+			},
+			{
+				"id": "de0f8b86-72d2-495d-b65a-9113df5f9997",
+				"name": "3rd iteration",
+				"description": "",
+				"earliestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"latestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"RS2duration": 86400000
+			},
+			{
+				"id": "75cb50bd-04cd-4d69-bf50-c986f26d2bb9",
+				"name": "4th iteration",
+				"description": "",
+				"earliestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 2,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"latestDateOfRef2": {
+					"years": 1993,
+					"months": 1,
+					"date": 3,
+					"hours": 6,
+					"minutes": 0,
+					"seconds": 0,
+					"milliseconds": 0
+				},
+				"RS2duration": 86400000
+			}
+		],
+		"active_universe_index": -1
+	}
+};
+
 module.exports = [
-    empty_preset, spock, marty, cooper
+    empty_preset,
+    spock,
+    marty,
+    cooper,
+    groundhog_day
 ]
