e4225e067a058ecd74d6253319dcae524d418379
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2017-10-29 02:10:31 +0200

    fix preset

--- presets/cooper.js
+++ presets/cooper.js
@@ -1,126 +1,134 @@
 module.exports = {
-    "name": "Cooper",
+	"name": "Cooper",
 	"data": {
-		"spans": [
+		"paths": [
 			{
-				"id": "c343ea92-3316-4ae7-8fc1-45f0a3a2a3f9",
-				"startTime": {
-					"years": 2060,
-					"months": 0,
-					"date": 9,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"endTime": {
-					"years": 2060,
-					"months": 0,
-					"date": 10,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"dilationFactor": 1,
-				"universe_index": 0,
-				"durationInRS1": 86400000,
+				"id": "8213d8e0-4bfa-4d2c-aa00-416b61376a04",
+				"name": "Cooper",
 				"description": "",
-				"isInactive": false,
-				"relativeStartRS1": 0,
-				"relativeEndRS1": 0.4241719930273097,
-				"relativeStartRS2": 0,
-				"relativeEndRS2": 0.00004560795402718234
-			},
-			{
-				"id": "2dbc6d97-ee63-4a60-ab9a-1a76c554f9b3",
-				"startTime": {
-					"years": 2060,
-					"months": 0,
-					"date": 10,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"endTime": {
-					"years": 2120,
-					"months": 0,
-					"date": 20,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"dilationFactor": 61320,
-				"universe_index": 0,
-				"durationInRS1": 30890958.90410959,
-				"description": "Due to his proximity to the black hole \"Gargantua\", one hour of Cooper's time equals 61320 hours of earth time.",
-				"isInactive": false,
-				"relativeStartRS1": 0.4241719930273097,
-				"relativeEndRS1": 0.5758280069726903,
-				"relativeStartRS2": 0.00004560795402718234,
-				"relativeEndRS2": 0.9999543920459728
-			},
-			{
-				"id": "1507eb59-72fb-4167-91d9-2537309361a9",
-				"startTime": {
-					"years": 2120,
-					"months": 0,
-					"date": 20,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"endTime": {
-					"years": 2120,
-					"months": 0,
-					"date": 21,
-					"hours": 0,
-					"minutes": 0,
-					"seconds": 0,
-					"milliseconds": 0
-				},
-				"dilationFactor": 1,
-				"universe_index": 0,
-				"durationInRS1": 86400000,
-				"description": "",
-				"isInactive": false,
-				"relativeStartRS1": 0.5758280069726903,
-				"relativeEndRS1": 1,
-				"relativeStartRS2": 0.9999543920459728,
-				"relativeEndRS2": 1
+				"RS1duration": 0,
+				"spans": [
+					{
+						"id": "c343ea92-3316-4ae7-8fc1-45f0a3a2a3f9",
+						"startTime": {
+							"years": 2060,
+							"months": 0,
+							"date": 9,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"endTime": {
+							"years": 2060,
+							"months": 0,
+							"date": 10,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"dilationFactor": 1,
+						"universe_index": 0,
+						"durationInRS1": 86400000,
+						"description": "",
+						"isInactive": false,
+						"relativeStartRS1": 0,
+						"relativeEndRS1": 0.4241719930273097,
+						"relativeStartRS2": 0,
+						"relativeEndRS2": 0.00004560795402718234
+					},
+					{
+						"id": "2dbc6d97-ee63-4a60-ab9a-1a76c554f9b3",
+						"startTime": {
+							"years": 2060,
+							"months": 0,
+							"date": 10,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"endTime": {
+							"years": 2120,
+							"months": 0,
+							"date": 20,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"dilationFactor": 61320,
+						"universe_index": 0,
+						"durationInRS1": 30890958.90410959,
+						"description": "Due to his proximity to the black hole \"Gargantua\", one hour of Cooper's time equals 61320 hours of earth time.",
+						"isInactive": false,
+						"relativeStartRS1": 0.4241719930273097,
+						"relativeEndRS1": 0.5758280069726903,
+						"relativeStartRS2": 0.00004560795402718234,
+						"relativeEndRS2": 0.9999543920459728
+					},
+					{
+						"id": "1507eb59-72fb-4167-91d9-2537309361a9",
+						"startTime": {
+							"years": 2120,
+							"months": 0,
+							"date": 20,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"endTime": {
+							"years": 2120,
+							"months": 0,
+							"date": 21,
+							"hours": 0,
+							"minutes": 0,
+							"seconds": 0,
+							"milliseconds": 0
+						},
+						"dilationFactor": 1,
+						"universe_index": 0,
+						"durationInRS1": 86400000,
+						"description": "",
+						"isInactive": false,
+						"relativeStartRS1": 0.5758280069726903,
+						"relativeEndRS1": 1,
+						"relativeStartRS2": 0.9999543920459728,
+						"relativeEndRS2": 1
+					}
+				]
 			}
 		],
 		"universes": [
 			{
-				"id": "9e87f820-99e7-4b9c-9395-c7ce053764da",
+				"id": "a4b25ddb-008b-4821-b80b-f161d18b766c",
 				"name": "",
-				"description": ""
+				"description": "",
+				"earliestDateOfRef2": {
+					"years": 2017,
+					"months": 9,
+					"date": 29,
+					"hours": 2,
+					"minutes": 8,
+					"seconds": 25,
+					"milliseconds": 451
+				},
+				"latestDateOfRef2": {
+					"years": 2017,
+					"months": 9,
+					"date": 29,
+					"hours": 2,
+					"minutes": 8,
+					"seconds": 25,
+					"milliseconds": 452
+				},
+				"RS2duration": 1,
+				"isCreatedAtFirstEntering": false
 			}
 		],
-		"id_counter": null,
-		"active_universe_index": -1,
-		"earliestDateOfRef2": {
-			"years": 2060,
-			"months": 0,
-			"date": 9,
-			"hours": 0,
-			"minutes": 0,
-			"seconds": 0,
-			"milliseconds": 0
-		},
-		"latestDateOfRef2": {
-			"years": 2120,
-			"months": 0,
-			"date": 21,
-			"hours": 0,
-			"minutes": 0,
-			"seconds": 0,
-			"milliseconds": 0
-		},
-		"RS1duration": 203690958.9041096,
-		"RS2duration": 1894406400000
+		"active_universe_index": 0,
+		"active_path_id": "8213d8e0-4bfa-4d2c-aa00-416b61376a04"
 	}
 };
